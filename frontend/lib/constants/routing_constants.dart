8a0ec9442b629bf294982dd2d95b956380b36580
Aditi-z <aditi.garg22501@gmail.com>
2023-01-11 23:08:07 +0530

    "New: logout route"

--- frontend/lib/constants/routing_constants.dart
+++ frontend/lib/constants/routing_constants.dart
@@ -2,5 +2,6 @@ const welcomeScreenRoute = "/";
 const myFriendsRoute = "/myFriends";
 const suggestFriendsRoute = "/suggestFriends";
 const appPurposeRoute = "/appPurpose";
+const logoutRoute = "/logout";
 const userPreferencesRoute = "/userPreferences";
 const onBoardingRoute = "/onBoarding";
